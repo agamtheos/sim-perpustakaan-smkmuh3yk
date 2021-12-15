5d33efd7f7cd19abdcca006faad1110b2df7cfcc
Agam Theos <agamtheos@gmail.com>
2021-12-15 10:22:49 +0700

    fix app.js

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ app.use(adminRoutes);
 app.use(bookRoutes);
 app.use(authRoutes);
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
